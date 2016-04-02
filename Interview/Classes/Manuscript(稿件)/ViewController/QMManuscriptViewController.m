8e47c2850652e04b8e9a2f8b3a06e8ce7f472759
fei <fei@ChengFei.local>
2016-04-02 10:21:19 +0800

    测试 git

--- Interview/Classes/Manuscript(稿件)/ViewController/QMManuscriptViewController.m
+++ Interview/Classes/Manuscript(稿件)/ViewController/QMManuscriptViewController.m
@@ -22,6 +22,8 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+//    test git 
 }
 
 - (void)didReceiveMemoryWarning {
